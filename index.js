7a46704e8795994088ef8708d3f17b312391bf12
justgu.us <hoeveguus@gmail.com>
2022-02-15 18:48:08 +0100

    added script to top

--- index.js
+++ index.js
@@ -95,3 +95,12 @@ function addChat(input, product) {
   )
 
 }
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+})
+}
